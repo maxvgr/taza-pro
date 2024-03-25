00107dfe543d5aa68d070defc22d7f6975696c5d
Max V Grushin <support@ids.spb.ru>
2024-03-25 12:56:15 +0300

    добавлен параллакс

--- src/assets/scripts/page/home.js
+++ src/assets/scripts/page/home.js
@@ -117,6 +117,14 @@ new Swiper(".trustus .swiper", {
     disableOnInteraction: false,
   },
 
-
 });
+
+let bg = document.querySelectorAll('.mouse-parallax-bg');
+for (let i = 0; i < bg.length; i++){
+    window.addEventListener('mousemove', function(e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;
+        bg[i].style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+    });
+}
 
