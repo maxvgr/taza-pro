c4b4f4fac99981578583051d2b91dd8997e7a9a6
Max V Grushin <support@ids.spb.ru>
2024-03-28 10:26:39 +0300

    адаптив

--- src/assets/scripts/partials/header.js
+++ src/assets/scripts/partials/header.js
@@ -2,3 +2,20 @@ import { SetSize } from "../base/functions";
 
 const header = document.getElementById("header");
 const headerHeight = SetSize(header, "header", true);
+
+
+
+const hamburger = document.getElementsByClassName("hamburger--spin")[0]
+const mobileNavs = document.getElementsByClassName("nav--mobile")[0]
+const mobileUl = document.getElementsByClassName("dropdown-mobile")[0]
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active")
+  mobileNavs.classList.toggle("is-active")
+})
+
+mobileUl.addEventListener("click", () => {
+  mobileUl.classList.toggle("is-active")
+})
+
